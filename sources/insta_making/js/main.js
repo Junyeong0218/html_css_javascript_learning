4d376ebbc148cbb835196e5f64f98e182f1c0316
Junyeong0218 <hippo2003@naver.com>
2021-12-24 22:44:23 +0900

    2021. 12. 24 수정4

--- sources/insta_making/js/main.js
+++ sources/insta_making/js/main.js
@@ -4,13 +4,36 @@ function moveMainRight() {
 }
 */
 
-window.onresize = function(event) {
-    var innerWidth = window.innerWidth - 334;
+window.onload = function(event) {
     var control_left = document.getElementById("control_margin_left");
-    if(innerWidth < 666) {
+    var main = document.getElementsByTagName("main")[0];
+    var contents = document.getElementById("adjust_main_contents");
+    if(window.innerWidth < 1000) {
         control_left.style.display = "none";
+        main.style.justifyContent = "center";
+        contents.className = "";
     } else {
+        var dif = (window.innerWidth - 1000) / 2;
         control_left.style.display = "block";
-        control_left.style.left = innerWidth + 'px';
+        control_left.style.left = (666 + dif) + 'px';
+        main.style.justifyContent = "flex-start";
+        contents.className = "contents";
+    }
+}
+
+window.onresize = function(event) {
+    var control_left = document.getElementById("control_margin_left");
+    var main = document.getElementsByTagName("main")[0];
+    var contents = document.getElementById("adjust_main_contents");
+    if(window.innerWidth < 1000) {
+        control_left.style.display = "none";
+        main.style.justifyContent = "center";
+        contents.className = "";
+    } else {
+        var dif = (window.innerWidth - 1000) / 2;
+        control_left.style.display = "block";
+        control_left.style.left = (666 + dif) + 'px';
+        main.style.justifyContent = "flex-start";
+        contents.className = "contents";
     }
 }
